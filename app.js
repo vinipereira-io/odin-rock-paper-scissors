b6cae5715dad6e8a39047064bae8badca1d6d702
Vini Pereira <viniciusxmp@gmail.com>
2023-01-06 09:15:07 -0300

    Detail the problem being solved and write steps to solve it

---
+++ app.js
@@ -0,0 +1,19 @@
+/*Problem: Create a logic to play the game "Rock, Paper, Scissors" between human and computer. 
+In this game players select one option among Rock, Paper and Scissors. Rock beats Scissors; Paper beats Rock and Scissors beats Paper. Equal selections tie.
+The human selection will be input, while the computer selection will be random.
+We need to compare selections to determine who gets the round point. In case of tie, nobody gets the point.
+It will be a 5-round so the first to score 5 points is the winner.*/
+
+//Get the computer selection with a random logic
+
+//Get the user selection with an input (prompt)
+
+//Process the user input in a case-insensitive way
+
+//Compare selections and report round result
+
+//Compute score at the end of each round
+
+//Create a loop to keep the game going until one player scores 5 points
+
+//Report game result once the 5-point score is reached
