08acdc0d224b7cd53ea7d1545f02b9a8cbe58f36
Vini Pereira <viniciusxmp@gmail.com>
2023-01-07 09:37:17 -0300

    Get computer and user selections, compare and report result of a round.

--- app.js
+++ app.js
@@ -4,13 +4,65 @@ The human selection will be input, while the computer selection will be random.
 We need to compare selections to determine who gets the round point. In case of tie, nobody gets the point.
 It will be a 5-round so the first to score 5 points is the winner.*/
 
-//Get the computer selection with a random logic
+//Get the computer selection with a random function
 
-//Get the user selection with an input (prompt)
+function getComputerSelection() {
+    const options = ['Rock', 'Paper', 'Scissors'];
 
-//Process the user input in a case-insensitive way
+    let randomSelection = options[Math.floor(Math.random() * options.length)];
+
+    return randomSelection
+}
+
+let computerSelection = getComputerSelection();
+
+//Get the user selection with an input (prompt) and process it in a case-insensitive way
+
+function getUserSelection() {
+    let userInput = prompt('Rock, paper or scissors... What\'s your choice?');
+
+    let userInputProcessed = userInput[0].toUpperCase() + userInput.slice(1).toLowerCase();
+
+    return userInputProcessed;
+}
+
+let userSelection = getUserSelection();
 
 //Compare selections and report round result
+
+function playRound(userSelection, computerSelection) {
+     let selections = [userSelection, computerSelection];
+
+     if (userSelection === computerSelection) {
+        result = 'Game Tie!';
+     } else if (selections.includes('Rock') && selections.includes('Paper')) {
+        if (userSelection === 'Paper') {
+            result = 'You Win! Paper beats Rock.';
+        } else{
+            result = 'You Lose! Paper beats Rock.';
+        }
+     } else if (selections.includes('Rock') && selections.includes('Scissors')) {
+        if (userSelection === 'Rock') {
+            result = 'You Win! Rock beats Scissors.';
+        } else{
+            result = 'You Lose! Rock beats Scissors.';
+        }
+     } else if (selections.includes('Paper') && selections.includes('Scissors')) {
+        if (userSelection === 'Scissors') {
+            result = 'You Win! Scissors beats Paper.';
+        } else{
+            result = 'You Lose! Scissors beats Paper.';
+        }
+     } else {
+        result = 'Something went terribly wrong.';
+     }
+
+     return result;
+}
+
+let resultDeclaration = playRound(userSelection, computerSelection);
+
+console.log(resultDeclaration);
 
 //Compute score at the end of each round
 
