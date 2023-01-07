28e3f20dc9c66e5c68c7f3da3a6035758cb2b56e
Vini Pereira <viniciusxmp@gmail.com>
2023-01-07 10:28:05 -0300

    Create loop to play for 5 rounds and then declare game result. Also organize code.

--- app.js
+++ app.js
@@ -2,7 +2,13 @@
 In this game players select one option among Rock, Paper and Scissors. Rock beats Scissors; Paper beats Rock and Scissors beats Paper. Equal selections tie.
 The human selection will be input, while the computer selection will be random.
 We need to compare selections to determine who gets the round point. In case of tie, nobody gets the point.
-It will be a 5-round so the first to score 5 points is the winner.*/
+It will be a 5-round so the player with higher score after 5 rounds is the game winner.*/
+
+let userScore = 0;
+
+let computerScore = 0;
+
+game();
 
 //Get the computer selection with a random function
 
@@ -14,8 +20,6 @@ function getComputerSelection() {
     return randomSelection
 }
 
-let computerSelection = getComputerSelection();
-
 //Get the user selection with an input (prompt) and process it in a case-insensitive way
 
 function getUserSelection() {
@@ -26,13 +30,7 @@ function getUserSelection() {
     return userInputProcessed;
 }
 
-let userSelection = getUserSelection();
-
-//Compare selections, report round result and computer game score
-
-let userScore = 0;
-
-let computerScore = 0;
+//Compare selections, report round result and update game score
 
 function playRound(userSelection, computerSelection) {
      let selections = [userSelection, computerSelection];
@@ -70,12 +68,30 @@ function playRound(userSelection, computerSelection) {
      return result;
 }
 
-let resultDeclaration = playRound(userSelection, computerSelection);
+//Create a loop to play 5 rounds
 
-console.log(resultDeclaration);
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let computerSelection = getComputerSelection();
 
-console.log(`Human: ${userScore} x Computer: ${computerScore}`);
+        let userSelection = getUserSelection();
 
-//Create a loop to keep the game going until one player scores 5 points
+        let resultDeclaration = playRound(userSelection, computerSelection);
 
-//Report game result once the 5-point score is reached
+        console.log(resultDeclaration);
+
+        console.log(`Human: ${userScore} x Computer: ${computerScore}`);
+    }
+}
+
+//Report game result after 5 rounds
+
+console.log('Game Over');
+
+if (userScore > computerScore) {
+    console.log(`You Win! Human: ${userScore} x Computer: ${computerScore}`);
+} else if (userScore < computerScore) {
+    console.log(`You Lose! Human: ${userScore} x Computer: ${computerScore}`);
+} else {
+    console.log(`Tie Game! Human: ${userScore} x Computer: ${computerScore}`);
+}
