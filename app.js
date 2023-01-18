abf049ffbbbf559fa41bf056b28fa76750972d81
Vini Pereira <viniciusxmp@gmail.com>
2023-01-18 19:12:11 -0300

    Refactor code to run on click button and end when a player reaches the 5-point score

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-/*Problem: Create a logic to play the game "Rock, Paper, Scissors" between human and computer. 
-In this game players select one option among Rock, Paper and Scissors. Rock beats Scissors; Paper beats Rock and Scissors beats Paper. Equal selections tie.
-The human selection will be input, while the computer selection will be random.
-We need to compare selections to determine who gets the round point. In case of tie, nobody gets the point.
-It will be a 5-round so the player with higher score after 5 rounds is the game winner.*/
-
 let userScore = 0;
 
 let computerScore = 0;
 
-//game();
+const buttons = document.querySelectorAll('button');
+
+const resultBoard = document.querySelector('#result-board');
+
+const scoreBoard = document.querySelector('#score-board');
+
+const finalBoard = document.querySelector('#final-board');
 
 //Get the computer selection with a random function
 
@@ -18,16 +18,6 @@ function getComputerSelection() {
     let randomSelection = options[Math.floor(Math.random() * options.length)];
 
     return randomSelection
-}
-
-//Get the user selection with an input (prompt) and process it in a case-insensitive way
-
-function getUserSelection() {
-    let userInput = prompt('Rock, paper or scissors... What\'s your choice?');
-
-    let userInputProcessed = userInput[0].toUpperCase() + userInput.slice(1).toLowerCase();
-
-    return userInputProcessed;
 }
 
 //Compare selections, report round result and update game score
@@ -68,38 +58,27 @@ function playRound(userSelection, computerSelection) {
      return result;
 }
 
-const buttons = document.querySelectorAll('button');
+//Announce a winner once one player reaches 5 points and reset score
 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        console.log(playRound(button.dataset.selection, getComputerSelection()));
-    });
-});
+function game(e) {
+    resultBoard.textContent = playRound(e.target.dataset.selection, getComputerSelection());
 
-//Create a loop to play 5 rounds
+    scoreBoard.textContent = `Human: ${userScore} x Computer: ${computerScore}`;
 
-/*function game() {
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerSelection();
+    if (userScore === 5 || computerScore === 5) {
+        if (userScore > computerScore) {
+            finalBoard.textContent = 'Game Over. You Win!';
+        } else {
+            finalBoard.textContent = 'Game Over. You Lose!';
+        }
 
-        let userSelection = getUserSelection();
+        let userScore = 0;
 
-        let resultDeclaration = playRound(userSelection, computerSelection);
-
-        console.log(resultDeclaration);
-
-        console.log(`Human: ${userScore} x Computer: ${computerScore}`);
+        let computerScore = 0;
     }
 }
 
-//Report game result after 5 rounds
-
-console.log('Game Over');
-
-if (userScore > computerScore) {
-    console.log(`You Win! Human: ${userScore} x Computer: ${computerScore}`);
-} else if (userScore < computerScore) {
-    console.log(`You Lose! Human: ${userScore} x Computer: ${computerScore}`);
-} else {
-    console.log(`Tie Game! Human: ${userScore} x Computer: ${computerScore}`);
-}*/
+//Play the game when an option is selected
+buttons.forEach((button) => {
+    button.addEventListener('click', game);
+});
